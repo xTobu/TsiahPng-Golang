fbd9f4cb442ab81db6b8cfbb830e22178dcf648a
Junxiang <junxiang.oriint@gmail.com>
2018-08-22 17:39:42 +0800

    wip on 2018-08-22 17:39:42

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	appstart "TsiahPng-Golang/AppStart"
-	"database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/appengine"
 )
 
-var db *sql.DB
-
 func main() {
-
 	appstart.RouteConfig()
 	appengine.Main()
 }
